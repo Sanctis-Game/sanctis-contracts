003b47a6fe2a60be69f52de9737a85d7a1e94f3e
Dodecahedr0x <hexadecifish@gmail.com>
2022-03-27 15:14:20 +0200

    feat: Deploy to testnet

--- deploy.py
+++ deploy.py
@@ -19,21 +19,27 @@ signer = Signer(
 
 contracts = [
     # (Contract Label, ContractPath:ContractName, ContractAddress)
-    ("SANCTIS", "src/Sanctis.sol:Sanctis"),
-    ("CREDITS", "src/SpaceCredits.sol:SpaceCredits"),
-    ("PARLIAMENT", "src/Parliament.sol:Parliament"),
-    ("COMMANDERS", "src/Commanders.sol:Commanders"),
-    ("STANDARDS", "src/GalacticStandards.sol:GalacticStandards"),
-    ("PLANETS", "src/Planets.sol:Planets"),
-    ("FLEETS", "src/Fleets.sol:Fleets"),
-    ("RACEREG", "src/RaceRegistry.sol:RaceRegistry"),
-    ("RESREG", "src/ResourceRegistry.sol:ResourceRegistry"),
-    ("INFRAREG", "src/InfrastructureRegistry.sol:InfrastructureRegistry"),
-    ("SHIPREG", "src/ShipRegistry.sol:ShipRegistry"),
-    ("HUMANS", "src/races/Humans.sol:Humans"),
-    ("IRON", "src/resources/Iron.sol:Iron"),
-    ("IRONEXTRACTOR", "src/infrastructures/Extractors.sol:Extractors"),
-    ("SPATIOPORT", "src/infrastructures/Spatioports.sol:Spatioports"),
+    ("sanctis", "src/Sanctis.sol:Sanctis"),
+    ("credits", "src/extensions/SpaceCredits.sol:SpaceCredits"),
+    ("parliament", "src/Parliament.sol:Parliament"),
+    ("commanders", "src/extensions/Commanders.sol:Commanders"),
+    ("planets", "src/extensions/Planets.sol:Planets"),
+    ("fleets", "src/extensions/Fleets.sol:Fleets"),
+    ("humans", "src/races/Humans.sol:Humans"),
+    ("energy", "src/resources/Energy.sol:Energy"),
+    ("iron", "src/resources/Resource.sol:Resource"),
+    ("deuterium", "src/resources/Resource.sol:Resource"),
+    ("silicon", "src/resources/Resource.sol:Resource"),
+    ("ironMines", "src/infrastructures/ResourceProducer.sol:ResourceProducer"),
+    ("siliconFurnaces", "src/infrastructures/ResourceProducer.sol:ResourceProducer"),
+    ("heavyWaterPlants", "src/infrastructures/ResourceProducer.sol:ResourceProducer"),
+    ("solarPanels", "src/infrastructures/PowerPlants.sol:PowerPlants"),
+    ("fusionPlants", "src/infrastructures/PowerPlants.sol:PowerPlants"),
+    ("spatioports", "src/infrastructures/Spatioports.sol:Spatioports"),
+    ("transporters", "src/ships/Ship.sol:Ship"),
+    ("scouts", "src/ships/Ship.sol:Ship"),
+    ("destroyers", "src/ships/Ship.sol:Ship"),
+    ("resourceWrapper", "src/utils/ResourceWrapper.sol:ResourceWrapper"),
 ]
 
 deployer = Deployer(
@@ -42,78 +48,233 @@ deployer = Deployer(
     contracts,
     is_legacy=True,  # for legacy transactions
     debug=True,  # if True, prints the calling commands and raw output
-    name="test1",
+    name="test2",
 )
 
 path = [
     # Format :
     # (ACTION_TYPE, CONTRACT_LABEL, [ARG, ... ])
     # Governance
-    (Deployer.DEPLOY, "SANCTIS", []),
+    (Deployer.DEPLOY, "sanctis", []),
     (
         Deployer.DEPLOY,
-        "CREDITS",
-        [],
+        "credits",
+        ["$sanctis"],
     ),
-    (Deployer.DEPLOY, "PARLIAMENT", ["$CREDITS", "$CREDITS"]),
-    (
-        Deployer.SEND,
-        "SANCTIS",
-        ["setGovernance", "$PARLIAMENT", keypair["public"], "$CREDITS"],
-    ),
-    # World
-    (Deployer.DEPLOY, "COMMANDERS", ["$SANCTIS"]),
-    (Deployer.DEPLOY, "STANDARDS", ["$SANCTIS"]),
-    (Deployer.DEPLOY, "PLANETS", ["$SANCTIS", "$CREDITS", "1000000000000000000000000"]),
-    (Deployer.DEPLOY, "FLEETS", ["$SANCTIS"]),
-    (
-        Deployer.SEND,
-        "SANCTIS",
-        ["setWorld", "$PLANETS", "$COMMANDERS", "$FLEETS", "$STANDARDS", "100"],
-    ),
-    # Registries
-    (Deployer.DEPLOY, "RACEREG", []),
-    (Deployer.DEPLOY, "RESREG", []),
-    (Deployer.DEPLOY, "INFRAREG", []),
-    (Deployer.DEPLOY, "SHIPREG", []),
-    (
-        Deployer.SEND,
-        "SANCTIS",
-        ["setRegistries", "$RACEREG", "$RESREG", "$INFRAREG", "$SHIPREG"],
-    ),
-    # Set up
-    (Deployer.DEPLOY, "HUMANS", ["$SANCTIS"]),
-    (Deployer.DEPLOY, "IRON", ["$SANCTIS"]),
+    (Deployer.DEPLOY, "parliament", ["$credits", keypair["public"]]),
+    # Extensions
+    (Deployer.DEPLOY, "commanders", ["$sanctis"]),
+    (Deployer.DEPLOY, "planets", ["$sanctis", "10000000000000000000"]),
+    (Deployer.DEPLOY, "fleets", ["$sanctis", "84600", "1000"]),
+    # Resource
+    (Deployer.DEPLOY, "humans", ["$sanctis"]),
+    (Deployer.DEPLOY, "energy", ["$sanctis"]),
+    (Deployer.DEPLOY, "iron", ["$sanctis", "Iron", "IRON"]),
+    (Deployer.DEPLOY, "deuterium", ["$sanctis", "Deuterium", "DEUT"]),
+    (Deployer.DEPLOY, "silicon", ["$sanctis", "Silicon", "SILI"]),
+    # Resource Infrastructures
     (
         Deployer.DEPLOY,
-        "IRONEXTRACTOR",
+        "ironMines",
         [
-            "$SANCTIS",
-            "1",  # Resource ID
-            "1000000000000000000",  # Reward base
-            "1000000000000000000",  # Reward rate
-            "100",  # Delay
-            "[[1,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]",  # Costs
-            "[[1,1000000],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]",  # Rates
+            "$sanctis",
+            "36",  # Delay
+            f"[$iron]",  # Rewards resources
+            f"[1000000000000000000]",  # Rewards base
+            f"[1000000000000000000]",  # Rewards rate
+            f"[$iron,$energy]",  # Costs Resources
+            f"[0,0]",  # Costs Base
+            f"[1000000000000000000,500000000000000000]",  # Costs Rates
         ],
     ),
-    # Add Humans
     (
-        Deployer.SEND,
-        "SANCTIS",
-        ["add", "0", "1"],
+        Deployer.DEPLOY,
+        "siliconFurnaces",
+        [
+            "$sanctis",
+            "1600",  # Delay
+            "[$silicon]",  # Reward resources
+            "[1000000000000000000]",  # Reward base
+            "[1000000000000000000]",  # Reward rate
+            "[$iron,$energy]",  # Costs Resources
+            "[1000000000000000000,10000000000000000]",  # Costs Base
+            "[1000000000000000000,10000000000000000]",  # Costs Rates
+        ],
     ),
-    # Add Iron
     (
-        Deployer.SEND,
-        "SANCTIS",
-        ["add", "1", "1"],
+        Deployer.DEPLOY,
+        "heavyWaterPlants",
+        [
+            "$sanctis",
+            "3600",  # Delay
+            "[$deuterium]",  # Reward resources
+            "[1000000000000000000]",  # Reward base
+            "[1000000000000000000]",  # Reward rate
+            "[$iron,$silicon,$energy]",  # Costs Resources
+            "[1000000000000000000,30000000000000000,30000000000000000]",  # Costs Base
+            "[1000000000000000000,30000000000000000,10000000000000000]",  # Costs Rates
+        ],
     ),
-    # Add Extractors
+    (
+        Deployer.DEPLOY,
+        "solarPanels",
+        [
+            "$sanctis",
+            "$energy",
+            "1000000000000000000",  # Reward base
+            "1000000000000000000",  # Reward rate
+            "360",  # Delay
+            "[$iron,$silicon]",  # Costs Resources
+            "[0,0]",  # Costs Base
+            "[1000000000000000000,10000000000000000]",  # Costs Rates
+        ],
+    ),
+    (
+        Deployer.DEPLOY,
+        "fusionPlants",
+        [
+            "$sanctis",
+            "$energy",
+            "1000000000000000000",  # Reward base
+            "1000000000000000000",  # Reward rate
+            "3600",  # Delay
+            "[$iron,$deuterium]",  # Costs Resources
+            "[1000000000000000000,1000000000000000000]",  # Costs Base
+            "[1000000000000000000,100000000000000000]",  # Costs Rates
+        ],
+    ),
+    # Fleets
+    (
+        Deployer.DEPLOY,
+        "spatioports",
+        [
+            "$sanctis",
+            "3600",  # Delay
+            "[$iron,$silicon]", # Costs Resources
+            "[1000000000000000000,1000000000000000000]",  # Costs base
+            "[1000000000000000000,10000000000000000]",  # Costs Rates
+        ],
+    ),
+    (
+        Deployer.DEPLOY,
+        "transporters",
+        [
+            "$sanctis",
+            "3600",  # Speed
+            "5",  # Offensive power
+            "100",  # Defensive power
+            "1000000000000000000",  # Capacity
+            "[$iron,$silicon]",  # Costs resources
+            "[1000000000000000000,1000000000000000000]",  # Costs base
+        ],
+    ),
+    (
+        Deployer.DEPLOY,
+        "scouts",
+        [
+            "$sanctis",
+            "7000",  # Speed
+            "0",  # Offensive power
+            "1",  # Defensive power
+            "0",  # Capacity
+            "[$iron,$silicon]",  # Costs resources
+            "[1000000000000000000,3000000000000000000]",  # Costs base
+        ],
+    ),
+    (
+        Deployer.DEPLOY,
+        "destroyers",
+        [
+            "$sanctis",
+            "3600",  # Speed
+            "250",  # Offensive power
+            "125",  # Defensive power
+            "0",  # Capacity
+            "[$iron,$silicon,$deuterium]",  # Costs resources
+            "[1000000000000000000,1000000000000000000,1000000000000000000]",  # Costs base
+        ],
+    ),
+    # Utils
+    (
+        Deployer.DEPLOY,"resourceWrapper",["$sanctis"],
+    ),
+    # Governance setup
     (
         Deployer.SEND,
-        "SANCTIS",
-        ["add", "2", "1"],
+        "sanctis",
+        ["setParliamentExecutor", keypair["public"]],
+    ),
+    # Insert extensions
+    (Deployer.SEND, "sanctis", ["insertAndAllowExtension", "$credits"]),
+    (Deployer.SEND, "sanctis", ["insertAndAllowExtension", "$commanders"]),
+    (Deployer.SEND, "sanctis", ["insertAndAllowExtension", "$planets"]),
+    (Deployer.SEND, "sanctis", ["insertAndAllowExtension", "$fleets"]),
+    # Allow
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$energy", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$iron", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$deuterium", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$silicon", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$ironMines", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$heavyWaterPlants", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$siliconFurnaces", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$spatioports", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$transporters", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$scouts", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$destroyers", "1"],
+    ),
+    (
+        Deployer.SEND,
+        "sanctis",
+        ["setAllowed", "$resourceWrapper", "1"],
+    ),
+    # Mint initial supply
+    (
+        Deployer.SEND,
+        "credits",
+        ["mint", keypair["public"], "10000000000000000000000000"],
     ),
 ]
 
